809f664964f61e8af6a41011fd84ec812c3b04f5
Rodrigo Carvalho Silva <me@rodsilva.com>
2026-03-25 21:42:15 -0300

    Revert "Approve regression tests, remove unrelated .gitignore change"
    This reverts commit fc17a8b8ebea55778fe07b11dcc635eff37af53b.

--- build2/CMakeFiles/pkgRedirects/pugixml-config.cmake
+++
@@ -1,2 +0,0 @@
-include("${CMAKE_CURRENT_LIST_DIR}/pugixml-extra.cmake" OPTIONAL)
-include("${CMAKE_CURRENT_LIST_DIR}/pugixmlExtra.cmake" OPTIONAL)
--- build2/_deps/getoptpp-subbuild/CMakeFiles/getoptpp-populate.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for getoptpp-populate.
-# This may be replaced when dependencies are built.
